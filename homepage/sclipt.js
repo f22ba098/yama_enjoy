c0f1517b7535f052e04b82cb798510786d9c25a1
f22ba098 <116255627+f22ba098@users.noreply.github.com>
2023-02-09 18:35:20 +0900

    Update sclipt.js

--- homepage/sclipt.js
+++ homepage/sclipt.js
@@ -3,7 +3,7 @@ function LoginPage() {
     let txt = document.getElementById("mail").value;
     let txt2 = document.getElementById("pass").value;
 
-    if (txt == "f22ba098@chuo.ac.jp" && txt2 == "890ab22F!dic") {
+    if (txt == "f22ba098@gmail.com" && txt2 == "yakiniku") {
         location.href = "top.html";
     } else {
         alert("メールアドレスまたはパスワードが違います。");
